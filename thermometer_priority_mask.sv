7b3327f056d5e8012bf57b77b7fbba16dbfe3125
Ishan Dalal <ishan9855@gmail.com>
2019-03-27 17:03:53 -0700

    Update thermometer_priority_mask.sv

--- thermometer_priority_mask.sv
+++ thermometer_priority_mask.sv
@@ -1,4 +1,3 @@
-
 module thermometer_priority_mask #(
   parameter request_lines = 4)
   (
@@ -13,22 +12,15 @@ input	logic								rst;
 input  logic 	[request_lines -1 : 0]		req;
 output logic	[request_lines - 1 : 0]		mask_req;
 
-  logic	[request_lines - 1 : 0]	mask;
+  initial begin 
+    	mask_req = '0;
+  end 
   
   always @(*) begin
-    mask = req ^ (req - 1);
-    mask = (req == {request_lines{1'b0}}) ? mask : ~mask;
-    mask = mask | req;
+    mask_req = req ^ (req - 1);
+    mask_req = (req == {request_lines{1'b0}}) ? mask_req : ~mask_req;
+    mask_req = mask_req | req;
   end
  
-  
-  always @(posedge clk or negedge rst)
-    	begin
-          if(!rst) 
-            	mask_req <= '0;
-           else
-             	mask_req <= mask;
-        end
-  
   
 endmodule
