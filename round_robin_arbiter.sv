747dfdd8aaa011b38149a91a965dc2315ac78921
Ishan Dalal <ishan9855@gmail.com>
2019-03-27 17:31:36 -0700

    Update round_robin_arbiter.sv

--- round_robin_arbiter.sv
+++ round_robin_arbiter.sv
@@ -18,24 +18,23 @@ module round_robin_arbiter #(
   logic	[request_lines - 1: 0]	masked_grant;
   
   priority_arbiter #(.request_lines(request_lines))
-  (	.rst		(rst),
+  normal_arbiter (	.rst		(rst),
    .clk			(clk),
    .req			(req),
    .grant		(normal_grant));
-
   
   thermometer_priority_mask #(.request_lines (request_lines))
-  (
+  thermometer_masking(
     .rst		(rst),
     .clk		(clk),
     .req		(grant),
     .mask_req	(masked_grant_previous));
   
-  assign masked_requests = req & normal_grant;
+  assign masked_requests = req & masked_grant_previous;
   
   
   priority_arbiter	#(.request_lines(request_lines))
-  (.rst		(rst),
+  masking_arbiter (.rst		(rst),
    .clk		(clk),
    .req		(masked_requests),
    .grant	(masked_grant));
@@ -47,7 +46,33 @@ module round_robin_arbiter #(
       grant <= (masked_grant == {request_lines{1'b0}}) ? normal_grant : masked_grant;
   end
   
+endmodule
+
+
+module thermometer_priority_mask #(
+  parameter request_lines = 4)
+  (
+	rst,
+    clk,
+    req,
+	mask_req
+);
+
+input	logic								clk;
+input	logic								rst;
+input  logic 	[request_lines -1 : 0]		req;
+output logic	[request_lines - 1 : 0]		mask_req;
+
+  initial begin 
+    	mask_req = '0;
+  end 
   
+  always @(*) begin
+    mask_req = req ^ (req - 1);
+    mask_req = (req == {request_lines{1'b0}}) ? mask_req : ~mask_req;
+    mask_req = mask_req | req;
+  end
+ 
   
 endmodule
 
@@ -67,52 +92,6 @@ input  logic 	[request_lines -1 : 0]		req;
 output logic	[request_lines - 1 : 0]		grant;
 
   
-  /*always @(posedge clk or negedge rst)
-    	begin
-          if(!rst) 
-            	grant <= '0;
-           else
-             	grant <= req & -req;
-        end
-  */
-  
-  assign grant = ~rst ? '0 : req & ~req;
-  
-endmodule
-  
-module thermometer_priority_mask #(
-  parameter request_lines = 4)
-  (
-	rst,
-    clk,
-    req,
-	mask_req
-);
-
-input	logic								clk;
-input	logic								rst;
-input  logic 	[request_lines -1 : 0]		req;
-output logic	[request_lines - 1 : 0]		mask_req;
-
-  logic	[request_lines - 1 : 0]	mask;
-  
-  always @(*) begin
-    mask = req ^ (req - 1);
-    mask = (req == {request_lines{1'b0}}) ? mask : ~mask;
-    mask = mask | req;
-  end
- 
-  
-  assign mask_req = !rst ? '0 : mask;
-  
-  /*
-  always @(posedge clk or negedge rst)
-    	begin
-          if(!rst) 
-            	mask_req <= '0;
-           else
-             	mask_req <= mask;
-        end
-  */
+  assign grant = ~rst ? '0 : req & -req;
   
 endmodule
